25480bdcb0e95496504a2719718ced755c89879c
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-29 20:04:51 +0700

    next auth ts config

---
+++ auth.config.d.ts
@@ -0,0 +1,9 @@
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
+  interface JWT {
+    /** OpenID ID Token */
+    role?: UserRole;
+  }
+}
