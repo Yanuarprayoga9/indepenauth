60709010fbd4e986c691405802535533584c0595
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-29 20:05:01 +0700

    add route reset

--- routes.ts
+++ routes.ts
@@ -1,4 +1,4 @@
 export const authRoutes = ["/auth/login", "/auth/register"];
-export const publicRoutes = ["/"];
+export const publicRoutes = ["/","/auth/reset"];
 export const apiAuthPrefix = "/api/auth";
 export const DEFAULT_ISLOGIN_REDIRECT = "/dashboard";
