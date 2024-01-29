fdedc77d72558da6334d3b406da21b16d0a57f34
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-30 06:58:24 +0700

    schema from new pass

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,11 +1,20 @@
-
 import * as z from "zod";
 
+export const NewPasswordSchema = z.object({
+  password: z.string().min(6, {
+    message: "Password min 6 is required",
+  }),
+  ConfPassword: z
+    .string()
+    .min(6, {
+      message: "Password min 6 is required",
+    })
+});
 
 export const ResetPasswordSchema = z.object({
   email: z.string().email({
     message: "Email Is Required",
-  })
+  }),
 });
 export const LoginSchema = z.object({
   email: z.string().email({
